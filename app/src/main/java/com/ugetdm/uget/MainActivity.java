530e0da5daa7d4e9442a7b37aab221b27eed6d54
C.H. Huang <plushuang.tw@gmail.com>
2019-02-10 05:04:41 +0800

    check WRITE_EXTERNAL_STORAGE permission on startup.

--- app/src/main/java/com/ugetdm/uget/MainActivity.java
+++ app/src/main/java/com/ugetdm/uget/MainActivity.java
@@ -1,9 +1,19 @@
 package com.ugetdm.uget;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.provider.DocumentFile;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -78,6 +88,21 @@ public class MainActivity extends AppCompatActivity {
 
     // ------------------------------------------------------------------------
     // save & restore
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        checkPermission();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // offline status
+        app.saveStatus();
+    }
 
     @Override
     protected void onPause() {
@@ -180,6 +205,165 @@ public class MainActivity extends AppCompatActivity {
                 app.switchDownloadAdapter();
             }
         });
+    }
+
+    // --------------------------------
+    // permission
+
+    private static final int REQUEST_WRITE_STORAGE = 112;
+    private static final int FILE_CHOOSER_CODE = 42;
+    private static final int FILE_CREATOR_CODE = 43;
+
+    protected void checkPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.message_permission_required);
+                //        .setTitle("Permission required");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        makeRequest();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            else {
+                makeRequest();
+            }
+        }
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                REQUEST_WRITE_STORAGE);
+    }
+
+    protected void runFileChooser() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.putExtra(Intent.EXTRA_TITLE, "Pick category json file");
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/*");
+            // Android doesn't support 'json', getMimeTypeFromExtension("json") return null
+            // intent.setType(MimeTypeMap.getSingleton().getMimeTypeFromExtension("zip"));
+            startActivityForResult(intent, FILE_CHOOSER_CODE);
+        }
+    }
+
+    protected void onFileChooserResult(Uri treeUri) {
+        ParcelFileDescriptor parcelFD;
+
+        grantUriPermission(getPackageName(), treeUri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        try {
+            parcelFD = getContentResolver().openFileDescriptor(treeUri, "r");
+        }
+        catch (Exception e) {
+            parcelFD = null;
+        }
+
+        String filename = DocumentFile.fromSingleUri(this, treeUri).getName();
+        if (parcelFD != null && app.core.loadCategory(parcelFD.detachFd()) != 0) {
+            app.categoryAdapter.notifyDataSetChanged();
+            app.stateAdapter.notifyDataSetChanged();
+            // --- Snackbar ---
+            Snackbar.make(findViewById(R.id.fab), R.string.message_file_load_ok, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            // --- Toast ---
+            //Toast.makeText(this, "load " + filename,
+            //        Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // --- Snackbar ---
+            Snackbar.make(findViewById(R.id.fab), R.string.message_file_load_fail, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            // --- Toast ---
+            //Toast.makeText(this, "Failed to load " + filename,
+            //        Toast.LENGTH_SHORT).show();
+        }
+
+        revokeUriPermission(treeUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    }
+
+    protected void runFileCreator() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/*");
+            // Android doesn't support 'json', getMimeTypeFromExtension("json") return null
+            startActivityForResult(intent, FILE_CREATOR_CODE);
+        }
+    }
+
+    protected void onFileCreatorResult(Uri treeUri) {
+        ParcelFileDescriptor parcelFD;
+
+        grantUriPermission(getPackageName(), treeUri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        try {
+            parcelFD = getContentResolver().openFileDescriptor(treeUri, "w");
+        }
+        catch (Exception e) {
+            parcelFD = null;
+        }
+
+        // String filename = DocumentFile.fromSingleUri(this, treeUri).getName();
+        if (parcelFD != null && app.saveNthCategory(app.nthCategory, parcelFD.detachFd()) != false) {
+            // --- Snackbar ---
+            Snackbar.make(findViewById(R.id.fab), R.string.message_file_save_ok, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            // --- Toast ---
+            //Toast.makeText(this, "save " + filename,
+            //        Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // --- Snackbar ---
+            Snackbar.make(findViewById(R.id.fab), R.string.message_file_save_fail, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            // --- Toast ---
+            //Toast.makeText(this, "Failed to save " + filename,
+            //        Toast.LENGTH_SHORT).show();
+        }
+
+        revokeUriPermission(treeUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+    }
+
+    //  @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent resultData) {
+        if (resultCode == RESULT_CANCELED)
+            return;
+        Uri  treeUri = resultData.getData(); // you can't use Uri.fromFile() to get path
+
+        DocumentFile docFile = DocumentFile.fromSingleUri(this, treeUri);
+        String name = docFile.getName();
+
+        switch (requestCode) {
+            case FILE_CHOOSER_CODE:
+                onFileChooserResult(treeUri);
+                break;
+
+            case FILE_CREATOR_CODE:
+                onFileCreatorResult(treeUri);
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
