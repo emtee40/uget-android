13a66dd544b5f5977cfd6c65ff9b7021ebd90af8
C.H. Huang <plushuang.tw@gmail.com>
2019-03-09 08:08:26 +0800

    In single selection mode, program clear selection when user press item in action menu.

--- app/src/main/java/com/ugetdm/uget/MainActivity.java
+++ app/src/main/java/com/ugetdm/uget/MainActivity.java
@@ -305,6 +305,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // --- selection mode ---
+        if (app.downloadAdapter.singleSelection)
+            app.downloadAdapter.clearChoices(true);
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
